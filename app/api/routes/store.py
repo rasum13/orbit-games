2f6dac75f260bd80c20bf9d910fdd33af17400be
rasum13 <rasum.subedi13@gmail.com>
2026-08-05 23:22:45 +0545

    change route function name from sop to store_page

--- app/api/routes/store.py
+++ app/api/routes/store.py
@@ -4,7 +4,7 @@ from app.core.templates import templates
 router = APIRouter()
 
 @router.get("/")
-async def shop(request: Request):
+async def store_page(request: Request):
     return templates.TemplateResponse(
         request=request,
         name="store.html"

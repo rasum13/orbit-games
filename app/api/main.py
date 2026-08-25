5f641f521c75fc3268766c09b7c3a2d5546829cf
rasum13 <rasum.subedi13@gmail.com>
2026-08-25 14:05:10 +0545

    add endpoint for home page and change prefix for store route to '/store'

--- app/api/main.py
+++ app/api/main.py
@@ -3,6 +3,6 @@ from app.api.routes import store, game, user, root
 
 api_router = APIRouter()
 api_router.include_router(root.router, prefix="", tags=["root"])
-api_router.include_router(store.router, prefix="", tags=["store"])
+api_router.include_router(store.router, prefix="/store", tags=["store"])
 api_router.include_router(game.router, prefix="/game", tags=["game"])
 api_router.include_router(user.router, prefix="/user", tags=["user"])
--- app/api/routes/root.py
+++ app/api/routes/root.py
@@ -3,6 +3,14 @@ from app.core.templates import templates
 
 router = APIRouter()
 
+@router.get("/")
+async def home_page(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="home.html",
+        context={}
+    )
+
 @router.get("/about")
 async def about_page(request: Request):
     return templates.TemplateResponse(
